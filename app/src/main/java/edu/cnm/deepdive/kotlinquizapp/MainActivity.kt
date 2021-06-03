29f9a7d781db34a875dabd13fc2bee4144190e59
Jan Gevaert <jan.gevaert@gmail.com>
2021-06-03 16:25:50 -0600

    Created a constant class to add all the questions.

--- app/src/main/java/edu/cnm/deepdive/kotlinquizapp/MainActivity.kt
+++ app/src/main/java/edu/cnm/deepdive/kotlinquizapp/MainActivity.kt
@@ -1,12 +1,34 @@
 package edu.cnm.deepdive.kotlinquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        val bt_start = findViewById<Button>(R.id.bt_start)
+        val et_name = findViewById<TextInputLayout>(R.id.et_name)
+
+        bt_start.setOnClickListener{
+            if (et_name.toString().isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/edu/cnm/deepdive/kotlinquizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,11 @@
+package edu.cnm.deepdive.kotlinquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+    }
+}
