2daee199893a7c4747e1aa1fdb0df934455f11f6
Jan Gevaert <jan.gevaert@gmail.com>
2021-06-04 10:37:11 -0600

    Trying to figure out error in starting emulator.

--- app/src/main/java/edu/cnm/deepdive/kotlinquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/edu/cnm/deepdive/kotlinquizapp/QuizQuestionsActivity.kt
@@ -2,10 +2,14 @@ package edu.cnm.deepdive.kotlinquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestions()
+        Log.d("Questions size", "${questionsList.size}")
     }
 }
